c35c71c025987b459dab4240724970e13a9431e0
surendra ediga <surendraediga@surendras-MacBook-Air.local>
2021-06-10 14:35:00 +0530

    first day

---
+++ day1.py
@@ -0,0 +1,21 @@
+
+# reverse a number
+a = input("a: ")
+b = input("b: ")
+
+c = a
+a = b
+b = c
+
+print("a: "+ a)
+print("b: "+b)
+
+# find a band name using user input
+
+cityname =input("enter cityname where you grew up?")
+petname = input("enter pen name ?")
+
+print("so your band name is "+cityname+petname)
+
+
+
